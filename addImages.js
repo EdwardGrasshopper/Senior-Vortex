8fd2622661a1798ea4206fb392c67a73abda9e93
Edward Arzumanov <edwardarzumanov@gmail.com>
2020-06-09 01:25:26 -0400

    finalizing Senior Vortex. Added all the img sources. Added example of vortex with 3 rings.

--- addImages.js
+++ addImages.js
@@ -5,11 +5,25 @@
 //   document.body.append(newElement)
 // }
 
-function addImage2() {
+function addImages() {
   var images = document.querySelectorAll("img");
 
   for (var i=5; i < 67; i++) {
     var currentImage = images[i];
-    document.body.append(currentImage);
+    // document.body.append(currentImage);
+    document.getElementById("images").appendChild(currentImage);   
+  }
+}
+
+function longImageString2() {
+  var images = document.querySelectorAll("img");
+  var imageString = ""
+  for (var i=5; i < 67; i++) {
+    var currentImage = images[i];
+    imageString.concat(currentImage)
+
+    if (i == 66) {
+      console.log(imageString)
+    }
   }
 }
